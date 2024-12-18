7fa83a3386b96f71dd6d7a5e359d778f08134c64
YoungParrot <youngparrotnft@gmail.com>
2024-12-17 21:21:06 -0800

    more updates

---
+++ src/components/Countdown.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import dayjs from "dayjs";
+
+const Countdown = ({ date }) => {
+  const targetDate = dayjs(date);
+  console.log({ date, targetDate });
+  const now = dayjs();
+  const difference = targetDate.diff(now);
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer); // Cleanup the timer on component unmount
+  }, []);
+
+  function calculateTimeLeft() {
+    const now = dayjs();
+    const difference = targetDate.diff(now); // Milliseconds difference
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      const totalSeconds = Math.floor(difference / 1000);
+      const totalMinutes = Math.floor(totalSeconds / 60);
+      const totalHours = Math.floor(totalMinutes / 60);
+      const totalDays = Math.floor(totalHours / 24);
+
+      timeLeft = {
+        days: totalDays, // Total number of days left
+        hours: totalHours % 24, // Remaining hours after days are calculated
+        minutes: totalMinutes % 60, // Remaining minutes
+        seconds: totalSeconds % 60, // Remaining seconds
+      };
+    }
+
+    return timeLeft;
+  }
+
+  return (
+    <div className="text-center">
+      <div className="flex justify-center space-x-4 mt-4">
+        {timeLeft.hours !== undefined ? (
+          <>
+            <div className="flex flex-col items-center min-w-16 md:min-w-20">
+              <div className="bg-gray-200 p-4 w-full rounded-md text-2xl md:text-4xl font-bold text-black">
+                {timeLeft.days}
+              </div>
+              <div className="text-metallic mt-2">DAYS</div>
+            </div>
+            <div className="flex flex-col items-center min-w-16 md:min-w-20">
+              <div className="bg-gray-200 p-4 w-full rounded-md text-2xl md:text-4xl font-bold text-black">
+                {timeLeft.hours}
+              </div>
+              <div className="text-metallic mt-2">HOURS</div>
+            </div>
+            <div className="flex flex-col items-center min-w-16 md:min-w-20">
+              <div className="bg-gray-200 p-4 w-full rounded-md text-2xl md:text-4xl font-bold text-black">
+                {timeLeft.minutes}
+              </div>
+              <div className="text-metallic mt-2">MINUTES</div>
+            </div>
+            <div className="flex flex-col items-center min-w-16 md:min-w-20">
+              <div className="bg-gray-200 p-4 w-full rounded-md text-2xl md:text-4xl font-bold text-black">
+                {timeLeft.seconds}
+              </div>
+              <div className="text-metallic mt-2">SECONDS</div>
+            </div>
+          </>
+        ) : (
+          <div className="text-2xl text-metallic">Ended</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
--- src/templates/MermaidVsSeaCreaturesGameTemplate.js
+++ src/templates/MermaidVsSeaCreaturesGameTemplate.js
@@ -12,6 +12,7 @@ import {
   KAIA_SCAN_URL,
   MERMAID_VS_SEA_CREATURES_GAME_ADDRESS,
 } from "@/utils/environment";
+import Countdown from "@/components/Countdown";
 
 const BET_TYPE = {
   mermaid: "mermaid",
@@ -27,8 +28,10 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
   const [totalRewards, setTotalRewards] = useState(0);
   const [mermaidTotalPower, setMermaidTotalPower] = useState(0);
   const [seaCreaturesTotalPower, setSeaCreaturesTotalPower] = useState(0);
+  const [roundStats, setRoundStats] = useState({});
+  const [playerStats, setPlayerStats] = useState({});
 
-  const fetchData = async () => {
+  const fetchRound = async () => {
     try {
       const gameContract = getContract({
         address: MERMAID_VS_SEA_CREATURES_GAME_ADDRESS,
@@ -40,11 +43,7 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
       const roundStats = await gameContract.read.getRoundStats([
         parseInt(roundId.toString()),
       ]);
-      const playerStats = await gameContract.read.getPlayerStats([
-        parseInt(roundId.toString()),
-        address,
-      ]);
-
+      setRoundStats(roundStats);
       setMermaidTotalPower(roundStats.totalBetMermaid.toString());
       setSeaCreaturesTotalPower(roundStats.totalBetSeaCreatures.toString());
       setTotalRewards(formatEther(roundStats.totalRewards.toString()));
@@ -53,22 +52,32 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
     }
   };
 
+  const fetchPlayer = async () => {
+    try {
+      const gameContract = getContract({
+        address: MERMAID_VS_SEA_CREATURES_GAME_ADDRESS,
+        abi: MermaidVsSeaCreaturesGame,
+        client: publicClient,
+      });
+      const roundId = await gameContract.read.getCurrentRoundIndex();
+      const playerStats = await gameContract.read.getPlayerStats([
+        parseInt(roundId.toString()),
+        address,
+      ]);
+      setPlayerStats(playerStats);
+    } catch (error) {
+      console.log("Fetch player failed", error);
+    }
+  };
+
   useEffect(() => {
     if (!isConnected) {
       return;
     }
 
-    fetchData();
+    fetchRound();
+    fetchPlayer();
   }, [isConnected]);
-
-  const fetchUserInfo = async () => {};
-
-  const handleAfterBet = async () => {
-    // call bet info API
-
-    // fetch user info
-    fetchUserInfo();
-  };
 
   const handleBet = async (type) => {
     try {
@@ -88,13 +97,19 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
           value: parseEther(betAmount.toString()),
         });
       }
-      await publicClient.waitForTransactionReceipt({
+      const transactionReceipt = await publicClient.waitForTransactionReceipt({
         hash: tx,
       });
 
-      toast.success("Bet successful");
+      if (transactionReceipt.transactionHash) {
+        toast.success("Bet successful");
 
-      handleAfterBet();
+        fetchRound();
+        fetchPlayer();
+      } else {
+        console.log("Bet failed", transactionReceipt);
+        toast.error("Bet failed");
+      }
     } catch (error) {
       console.log("Bet failed", error);
       if (error.message.startsWith("User rejected the request")) {
@@ -140,16 +155,19 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
       <p className="text-4xl font-bold text-highlight py-4">
         Mermaid Vs Sea Creatures Game
       </p>
+      {roundStats.endTime && (
+        <Countdown date={roundStats.endTime.toString() * 1000} />
+      )}
       <div className="flex flex-col items-center gap-2 max-w-lg py-4">
-        <p className="text-xl font-bold text-highlight">Total Rewards</p>
+        <p className="text-2xl font-bold text-highlight">Total Rewards</p>
         <div className="flex">
-          <p className="text-xl text-metallic">{totalRewards} KAIA</p>
+          <p className="text-2xl text-metallic">{totalRewards} KAIA</p>
         </div>
       </div>
       <div className="flex justify-between gap-4 max-w-lg">
-        <p className="text-xl text-metallic">{mermaidTotalPower}</p>
-        <p>vs</p>
-        <p className="text-xl text-metallic">{seaCreaturesTotalPower}</p>
+        <p className="text-2xl text-highlight">{mermaidTotalPower}</p>
+        <p className="text-metallic">vs</p>
+        <p className="text-2xl text-highlight">{seaCreaturesTotalPower}</p>
       </div>
       <Image
         src="/images/mermaid-vs-sea-creatures.png"
@@ -167,23 +185,47 @@ const MermaidVsSeaCreaturesGameTemplate = () => {
           placeholder="Enter amount"
           className="block w-full px-4 py-2 mt-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-700"
         />
-        <p className="mt-4">KAIA</p>
+        <p className="mt-4 text-metallic">KAIA</p>
       </div>
-      <div className="flex justify-between gap-4 max-w-lg py-4">
-        <button
-          onClick={() => handleBet(BET_TYPE.mermaid)}
-          className="bg-primary text-white font-bold py-2 px-4 rounded"
-        >
-          Power Up Mermaid
-        </button>
-        <button
-          onClick={() => handleBet(BET_TYPE.seaCreatures)}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        >
-          Power Up Sea Creatures
-        </button>
-      </div>
-      {/* <button onClick={() => handleClaimReward()}>Claim Reward</button> */}
+      {isConnected ? (
+        <>
+          <div className="flex justify-between gap-4 max-w-lg py-4">
+            <button
+              onClick={() => handleBet(BET_TYPE.mermaid)}
+              className="bg-primary text-white font-bold py-2 px-4 rounded"
+            >
+              Power Up Mermaid
+            </button>
+            <button
+              onClick={() => handleBet(BET_TYPE.seaCreatures)}
+              className="bg-primary text-gray-700 font-bold py-2 px-4 rounded"
+            >
+              Power Up Sea Creatures
+            </button>
+          </div>
+          {playerStats[0] && (
+            <div>
+              <p className="text-highlight">
+                Your total power:{" "}
+                <span className="text-metallic">
+                  {playerStats[1].toString()}
+                </span>
+              </p>
+              <p className="text-highlight">
+                Your total spent:{" "}
+                <span className="text-metallic">
+                  {formatEther(playerStats[0].toString())} KAIA
+                </span>
+              </p>
+            </div>
+          )}
+          {/* <button onClick={() => handleClaimReward()}>Claim Reward</button> */}
+        </>
+      ) : (
+        <div className="mt-4">
+          <ConnectButton />
+        </div>
+      )}
     </div>
   );
 };
