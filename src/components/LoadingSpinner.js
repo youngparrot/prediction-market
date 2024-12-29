04f6f2321d23af5a9ea07cbe43a5525aa4acc50c
YoungParrot <youngparrotnft@gmail.com>
2024-12-28 16:54:51 -0800

    update loading color

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -5,7 +5,7 @@ const LoadingSpinner = () => {
     <div className="flex justify-center items-center">
       <div className="relative">
         <div className="w-16 h-16 border-4 border-gray-200 rounded-full"></div>
-        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-t-4 rounded-full animate-spin absolute top-0 left-0"></div>
+        <div className="w-16 h-16 border-4 border-accent border-t-transparent border-t-4 rounded-full animate-spin absolute top-0 left-0"></div>
       </div>
     </div>
   );
